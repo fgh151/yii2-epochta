e3fd4b65d29897320bb85195a0f46aaa6a5e79c8
Fedor B Gorsky <fedor@support-pc.org>
2016-01-29 14:01:31 +0300

    Init commit

---
+++ Module.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace fgh151\modules\epochta;
+
+/**
+ * Created by PhpStorm.
+ * User: fgorsky
+ * Date: 29.01.16
+ * Time: 13:53
+ */
+class Module
+{
+    public $sms_key_private='';
+    public $sms_key_public='';
+    public $URL_GAREWAY='http://atompark.com/api/sms/';
+    public $testMode=false;
+}
